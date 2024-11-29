7aa0b4cefd608bdf483710161c71f1170b493350
Alexis Leite <alexisleite.uy@gmail.com>
2024-11-29 02:12:48 -0300

    test

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,8 @@
 import { Pool } from 'pg';
+import { loadEnvConfig } from '@next/env';
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
 
 // Configure PostgreSQL connection
 export const pool = new Pool({
