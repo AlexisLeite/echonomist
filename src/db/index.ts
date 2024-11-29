31129a0a7f81f1f1f5bfafb52eaa5da5e1ed5b4f
Alexis Leite <alexisleite.uy@gmail.com>
2024-11-29 02:31:30 -0300

    test

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,10 +6,6 @@ loadEnvConfig(projectDir);
 
 // Configure PostgreSQL connection
 export const pool = new Pool({
-  user: process.env.DBUSER,
-  host: process.env.DBHOST,
-  database: process.env.DBNAME,
-  password: process.env.DBPASS,
-  port: parseInt(process.env.DBPORT!),
+  connectionString: process.env.POSTGRES_URL,
 });
 
